8f399c0a9545a55cce34c4c73741a8358e0e4f6d
Gwean <georgina.mondino1@gmail.com>
2022-04-03 17:40:04 -0300

    Va queriendo el LI

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,60 +1,68 @@
-import React, { Component } from 'react'
-import data from './data.json'
-import '../index.css'
-import Historia from './Historia';
-import Opciones from './Opciones';
+import React, { Component } from "react";
+import data from "./data.json";
+import "../index.css";
+import Historia from "./Historia";
+import Opciones from "./Opciones";
+import Recordatorio from "./Recordatorio";
 
 export default class Layout extends Component {
   constructor() {
     super();
     this.state = {
-        historia: data,
-        contador : 0,
-        historial : []
-        
-    }
+      historia: data,
+      contador: 0,
+      historial: [],
+      utlimoState : ""
+    };
   }
 
-componentDidMount(){
+  componentDidMount() {
     alert("Me acabo de crear");
-}
+  }
 
-handleClick = (clickEvento) => {
+  handleClick = (clickEvento) => {
     let cont = 0;
     let temp = [];
+    
     const id = clickEvento.target.id;
-    if (id === "A"){
-      cont ++;
+    if (this.state.utlimoState === "A") {
+      if (id === "A") {
+        cont += 2;
+      } else if (id === "B") {
+        cont += 3;
+      }
+    } else if (id === "A") {
+      cont++;
     } else if (id === "B") {
-      cont +=2;
+      cont += 2;
     }
-    temp =  this.state.historial;
-    temp.push(id)
+    temp = this.state.historial;
+    temp.push(id);
     this.setState({
-      contador : this.state.contador + cont,
-      historial : temp
-    })
-
-}
+      contador: this.state.contador + cont,
+      historial: temp,
+      utlimoState : id
+    });
+  };
   render() {
-      //Estructura basica Layout > 
-                                //historia, 
-                                //opciones > a y b, 
-                                //recordatorio
+    //Estructura basica Layout >
+    //historia,
+    //opciones > a y b,
+    //recordatorio
     return (
-      <div className='layout'>
-        <Historia 
-          historia = {this.state.historia[this.state.contador].historia}
+      <div className="layout">
+        <Historia
+          historia={this.state.historia[this.state.contador].historia}
         />
         <Opciones
-         opciones = {this.state.historia[this.state.contador].opciones}
-         click = {this.handleClick}
+          opciones={this.state.historia[this.state.contador].opciones}
+          click = {this.handleClick}
         />
-
+        <Recordatorio
+        recordatorio = { this.state.utlimoState}
+        historial = {this.state.historial}
+        />
       </div>
-     
-    )
+    );
   }
 }
-
-
---
+++ src/components/Recordatorio.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+export default class Recordatorio extends Component {
+
+  render() {
+      
+    return (
+      <div className="recordatorio">
+        <h3>{"Seleccion anterior: " + this.props.recordatorio }</h3>
+        <h4> Historial de opciones elegidas: </h4>
+        <ul>
+        {this.state.historial.map(eleccion => (
+            <li/>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
