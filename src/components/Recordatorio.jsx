171eb76cea1726164f3ddb7b01dad393313c40ff
Gwean <georgina.mondino1@gmail.com>
2022-04-03 17:56:10 -0300

    Va queriendo x2

--- src/components/Recordatorio.jsx
+++ src/components/Recordatorio.jsx
@@ -9,10 +9,8 @@ export default class Recordatorio extends Component {
         <h3>{"Seleccion anterior: " + this.props.recordatorio }</h3>
         <h4> Historial de opciones elegidas: </h4>
         <ul>
-        {this.state.historial.map(eleccion => (
-            <li/>
-          ))}
-        </ul>
+            {this.state.historial.map(eleccion => (<li>{eleccion}<li/>))}
+            </ul>
       </div>
     );
   }
